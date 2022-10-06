8bf2a06c81fc1ee3ca8d7e4cbbd606bbf8c41d32
Nam Tonthat <ntonthat@squareup.com>
2022-10-06 19:31:42 +1100

    Add in calendar functions

--- healthlake-calendar.py
+++ healthlake-calendar.py
@@ -4,6 +4,101 @@ import glob
 import os
 import pandas as pd
 
+from ics import Calendar, Event
+
+import logging
+logging.basicConfig()
+LOGGER = logging.getLogger(__name__)
+LOGGER.setLevel(logging.INFO)
+
+
+#  Calendar functions
+def make_event_name(event_type, description):
+    """
+    Creates an event name with an emoticon
+    """
+    emoticons = {
+        'sleep'     : "💤",
+        'activity'  : "🔥",
+        'food'      : "🥞",
+        'mindful'   : "🧘",
+        'exercise'  : "🏃",
+        'weight'    : "🎚️",
+        'average'   : "📈"
+    }
+
+    emoticon = emoticons.get(event_type)
+
+    # case statement to catch events that were not completed
+    if description == 1:
+        description = ""
+
+    event_name = f"{emoticon} {description}"
+    return event_name
+
+# %%
+def create_event(date, event_name, description: None):
+    """
+    Create an all day event for the given date and type
+    :param date: date as type datetime.date
+    :param event_name: name of event as string
+    """
+    all_day_date = f"{date} 00:00:00"
+    e = Event()
+    e.name = event_name
+    e.description = description
+    e.begin = all_day_date
+    e.end = all_day_date
+    e.make_all_day()
+
+    return e
+
+def make_description(row, event_type):
+    """Create a description field for the event"""
+
+    if event_type in ('sleep', 'activity'):
+        time = row['qty']
+        hours = int(time)
+        minutes = int((time - hours) * 60)
+
+        value = f"{hours} hours {minutes} mins"
+
+
+    # elif event_type == 'food'
+
+    description = f"{make_event_name(event_type)} {value}"
+
+    return description
+
+def generate_calendar(df):
+    """
+    Generates a CSV and ICS from the dataframe
+    :param df: cleansed dataframe from `create_description_cols`
+    :param outputs: as type string - a combination of both the local and public storage
+    """
+
+    # output_csv_path = f"{output_path}/{file_name}.csv"
+    # calendar_file_name = f'{file_name}.ics'
+    file_name = 'apple_health'
+
+    csv_file_name = f"{file_name}.csv"
+    ics_file_name  = f"{file_name}.ics"
+
+
+    LOGGER.info("Generating calendar (as .ICS)")
+    c = Calendar()
+    for _, row in df.iterrows():
+        e = create_event(row['date'], row['name'], row['dsc'])
+        c.events.add(e)
+
+    df.to_csv(csv_file_name, index=False)
+
+    with open(ics_file_name, 'w') as f:
+        f.write(str(c))
+        f.close()
+
+    LOGGER.info("Outputing CSV and ICS to: %s", csv_file_name)
+    return
 
 def convert_kj_to_cal(row, new_name):
     """Converts kj to calories"""
