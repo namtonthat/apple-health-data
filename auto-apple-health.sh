ef44c9268a79e9bb5059b9cf2a10830f749b72e4
Nam Tonthat <n.nam.tonthat@gmail.com>
2022-12-01 19:35:56 +1100

    Remove non required files

--- auto-apple-health.sh
+++
@@ -1,6 +0,0 @@
-# Automate run every day
-# adjust schedule using crontab -e
-conda activate apple-health
-cd ~/Documents/scratch/apple-health-calendar
-export AWS_DEFAULT_PROFILE=personal
-python3 apple-health.py
