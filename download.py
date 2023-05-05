15fed0e848beb9084471845450037f369e986e72
Nam Tonthat <n.nam.tonthat@gmail.com>
2023-05-05 21:36:03 +1000

    feat(sync-s3-data.yml): add environment variables for AWS access key and secret access key to download script fix(download.py): replace secrets with environment variables for AWS access key and secret access key to download parquet file from S3

--- download.py
+++ download.py
@@ -1,11 +1,11 @@
 import conf
 import s3fs
+import os
 
 
 def download_parquet_file(bucket_name, file_path, local_path):
     s3 = s3fs.S3FileSystem(
-        key={{secrets.AWS_ACCESS_KEY_ID}},
-        secret={{secrets.AWS_SECRET_ACCESS_KEY}},
+        key=os.getenv("aws_access_key_id"), secret=os.getenv("aws_secret_access_key")
     )
     with s3.open(f"{bucket_name}/{file_path}", "rb") as s3_file:
         with open(local_path, "wb") as local_file:
