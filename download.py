fc41b4ab76fcb4033ed215e8ab6651061fceaa9f
Nam Tonthat <n.nam.tonthat@gmail.com>
2023-05-05 21:29:33 +1000

    feat(sync-s3-data.yml): add GitHub Actions workflow to sync S3 data daily and on demand feat(download.py): add AWS credentials to S3FileSystem constructor to enable downloading from S3 bucket

--- download.py
+++ download.py
@@ -3,16 +3,16 @@ import s3fs
 
 
 def download_parquet_file(bucket_name, file_path, local_path):
-    s3 = s3fs.S3FileSystem()
+    s3 = s3fs.S3FileSystem(
+        key={{secrets.AWS_ACCESS_KEY_ID}},
+        secret={{secrets.AWS_SECRET_ACCESS_KEY}},
+    )
     with s3.open(f"{bucket_name}/{file_path}", "rb") as s3_file:
         with open(local_path, "wb") as local_file:
             local_file.write(s3_file.read())
 
 
 if __name__ == "__main__":
-    file_path = "outputs/latest_data.parquet"
-    local_path = "latest_data.parquet"
-
     download_parquet_file(
         bucket_name=conf.bucket,
         file_path=conf.latest_parquet_file_path,
