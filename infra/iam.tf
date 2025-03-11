869c40dac285bc1ebab36c1367a4094ff29ccb0c
Nam Tonthat <n.nam.tonthat@gmail.com>
2025-03-11 21:24:53 +1100

    feat: enable dbt tf

--- infra/iam.tf
+++ infra/iam.tf
@@ -10,17 +10,17 @@ resource "aws_iam_role" "lambda_ingest_role" {
   })
 }
 
-# resource "aws_iam_role" "lambda_dbt_role" {
-#   name = "lambda_dbt_role"
-#   assume_role_policy = jsonencode({
-#     Version = "2012-10-17",
-#     Statement = [{
-#       Action    = "sts:AssumeRole",
-#       Effect    = "Allow",
-#       Principal = { Service = "lambda.amazonaws.com" }
-#     }]
-#   })
-# }
+resource "aws_iam_role" "lambda_dbt_role" {
+  name = "lambda_dbt_role"
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17",
+    Statement = [{
+      Action    = "sts:AssumeRole",
+      Effect    = "Allow",
+      Principal = { Service = "lambda.amazonaws.com" }
+    }]
+  })
+}
 
 # IAM Policy Attachments for Lambda Functions
 resource "aws_iam_role_policy_attachment" "lambda_ingest_basic_execution" {
@@ -46,11 +46,11 @@ resource "aws_iam_role_policy_attachment" "lambda_ingest_s3_attach" {
   policy_arn = aws_iam_policy.lambda_ingest_s3_policy.arn
 }
 
-# resource "aws_iam_role_policy_attachment" "lambda_dbt_basic_execution" {
-#   role       = aws_iam_role.lambda_dbt_role.name
-#   policy_arn = "arn:aws:iam::aws:policy/service-role/AWSLambdaBasicExecutionRole"
-# }
-#
+resource "aws_iam_role_policy_attachment" "lambda_dbt_basic_execution" {
+  role       = aws_iam_role.lambda_dbt_role.name
+  policy_arn = "arn:aws:iam::aws:policy/service-role/AWSLambdaBasicExecutionRole"
+}
+
 
 
 
