a3b77a372a64558834668f0a1ee4ab786ecb2033
Nam Tonthat <n.nam.tonthat@gmail.com>
2025-03-11 15:37:25 +1100

    feat: unload raw data to s3 path

--- transforms/models/raw_api_metrics.sql
+++ transforms/models/raw_api_metrics.sql
@@ -1,6 +1,14 @@
+{{
+    config(
+        post_hook=[
+            "COPY {{ this }} TO 's3://{{ var('s3_bucket') }}/raw/raw_api_metrics.parquet'"
+        ]
+    )
+}}
+
 select
     cast(unnest(data_fields).date as date) as metric_date,
     metric_name,
     units,
-    unnest(data_fields).qty as quantity
-from {{ ref('stg_api_metrics') }};
+    round(unnest(data_fields).qty, 2) as quantity
+from {{ ref('stg_api_metrics') }}
