d41e05251caab79656e9e6e96798567650340d02
Nam Tonthat <n.nam.tonthat@gmail.com>
2025-03-24 01:37:45 +1100

    feat: enable hevy transforms

---
+++ transforms/models/staging/stg_exercise.sql
@@ -0,0 +1,29 @@
+with raw_data as (
+    select * from
+        read_json('s3://{{ var("s3_bucket") }}/exercise/*.json')
+),
+
+unnested_data as (
+    select
+        id,
+        title,
+        start_time,
+        end_time,
+        updated_at,
+        created_at,
+        unnest(exercises, recursive := true) 
+    from raw_data
+)
+
+select
+    id,
+    title,
+    start_time,
+    end_time,
+    updated_at,
+    created_at,
+    unnest(sets).type as set_type,
+    unnest(sets).weight_kg as weight_kg,
+    unnest(sets).reps as reps,
+    unnest(sets).rpe as rpe
+from unnested_data
