caef332921d68b8c2c8ad8d3e74fb37daca71a50
Nam Tonthat <n.nam.tonthat@gmail.com>
2025-03-24 23:39:27 +1100

    feat; add kpis volumes

--- transforms/models/semantic/semantic_exercises_kpis.sql
+++ transforms/models/semantic/semantic_exercises_kpis.sql
@@ -10,11 +10,40 @@ with volume_data as (
     from {{ ref('semantic_exercises') }}
     group by all
     order by start_time desc, index asc
+),
+
+total_workout_volume as (
+    select
+        metric_date,
+        start_time,
+        'workout_volume' as metric_name,
+        sum(volume_kg) as quantity,
+        'kg' as units
+    from volume_data
+    group by all
+    order by start_time desc
+),
+
+total_time as (
+    select distinct
+        id,
+        metric_date,
+        'workout_time' as metric_name,
+        workout_duration_mins as quantity,
+        'mins' as units
+    from volume_data
 )
 
 select
     metric_date,
-    exercise_name,
-    workout_duration_mins,
-    volume_kg
-from volume_data
+    metric_name,
+    quantity,
+    units
+from total_workout_volume
+union all
+select
+    metric_date,
+    metric_name,
+    quantity,
+    units
+from total_time
